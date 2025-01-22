5f795dbd1cda69393a40f24089023517ac0b6e3f
Dylan Ferrow <ferrow.dylan.code@gmail.com>
2025-01-22 11:33:30 +0000

    wip: data + footer

--- database/hotel_rooms.sql
+++ database/hotel_rooms.sql
@@ -2,7 +2,7 @@ CREATE TABLE `rooms` (
   `room_no` INT(11) NOT NULL,
   `type` ENUM('Standard','Dobble','Triple','Suite','Dulux','Connecting') NOT NULL,
   `Beds` VARCHAR(36) NOT NULL,
+  `people` VARCHAR(10) NOT NULL,-- low max - high max
   `cleaned` BOOLEAN NOT NULL DEFAULT FALSE,
   PRIMARY KEY (`room_no`)
-
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
