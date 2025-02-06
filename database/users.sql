03bcc751ce163ab917666af21d86ee084ddcbba7
Dylan Hughes <40012227@petroc.ac.uk>
2025-02-06 10:30:20 +0000

    fix: sql

--- database/users.sql
+++ database/users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `users`(
-    `id`         VARCHAR(36) NOT NULL auto_increment,
+    `id`         VARCHAR(36) NOT NULL ,
     `username`   VARCHAR(50) NOT NULL,
     `email`      VARCHAR(100) NOT NULL,
     `password`   VARCHAR(255) NOT NULL,
