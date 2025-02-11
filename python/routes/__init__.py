e651ed180b0fce76f82048f69a6b4d7256412433
dylan <ferrow.dylan.code@gmail.com>
2025-02-11 22:58:35 +0000

    feat: sessions

--- python/routes/__init__.py
+++ python/routes/__init__.py
@@ -4,22 +4,62 @@ from routes.genral import genral_router
 from routes.payment import payment_router
 from routes.book import Define as Define_book
 from routes.rooms import room_router
+from scripts.session import create, check, check_auth,clear
+import jwt
 
 
 def register_routes(app, DB):
 
     @app.before_request
     def before_request():
+        session_id = request.cookies.get('session_id')
+        if not session_id:
+            session_id = create(DB, request.remote_addr, request.user_agent.string)
+            response = jsonify({'message': 'create session'})
+            response.set_cookie('session_id', session_id)
+            return response, 102
+        if not check(DB, session_id, request.remote_addr, request.user_agent.string):
+            clear(DB, session_id)
+            response = jsonify({'message': 'ip address or user agent changed'})
+            response.delete_cookie('session_id')
+            response.delete_cookie('token')
+            response.set_cookie('auth', 'false')
+            session_id = create(DB, request.remote_addr, request.user_agent.string)
+            response.set_cookie('session_id', session_id)
+            response.headers['Location'] = '/login'
+            response.status_code = 302
+            return response
         if request.path in ['/dashboard', "/orders", "/settings", "/bookings"]:
             token = request.cookies.get('token')
             if not token:
                 return jsonify({'message': 'Missing token'}), 401
-            # try:  # TODO get secret from db
-            #     jwt.decode(token, 'secret', algorithms=['HS256'])
-            # except jwt.ExpiredSignatureError:
-            #     return jsonify({'message': 'Expired token'}), 401
-            # except jwt.InvalidTokenError:
-            #     return jsonify({'message': 'Invalid token'}), 401
+            token_secret = check_auth(DB, session_id, token)
+            
+            try: 
+                jwt.decode(token, token_secret, algorithms=['HS256'])
+            except jwt.ExpiredSignatureError:
+                clear(DB, session_id)
+                response = jsonify({'message': 'Expired token'})
+                response.delete_cookie('token')
+                response.delete_cookie('session_id')
+                response.set_cookie('auth', 'false')
+                session_id = create(DB, request.remote_addr, request.user_agent.string)
+                response.set_cookie('session_id', session_id)
+                response.headers['Location'] = '/login'
+                response.status_code = 302
+                return response
+            except jwt.InvalidTokenError:
+                clear(DB, session_id)
+                response = jsonify({'message': 'Invalid token'})
+                response.delete_cookie('token')
+                response.delete_cookie('session_id')
+                response.set_cookie('auth', 'false')
+                session_id = create(DB, request.remote_addr, request.user_agent.string)
+                response.set_cookie('session_id', session_id)
+                response.headers['Location'] = '/login'
+                response.status_code = 302
+                return response
+
 
     app.register_blueprint(Define_auth(DB))
     app.register_blueprint(Define_book(DB))
--- python/scripts/session.py
+++ python/scripts/session.py
@@ -1,7 +1,5 @@
 # TODO complete + import and add to before_request and nessary places
 import uuid
-import jwt
-import datetime
 
 def create(DB, ip_address, user_agent):
     session_id = str(uuid.uuid4())
@@ -9,21 +7,38 @@ def create(DB, ip_address, user_agent):
               ["session_id", "previous_session_id", "login_status", "ip_address", "user_agent"])
     return session_id
 
-def check(DB, session_id):#TODO
+def clear(DB, session_id):
+    DB.update("sessions", "login_status = 0, token = NULL", f"session_id = '{session_id}'")
+
+
+def check(DB, session_id,ip_address,user_agent):#TODO
     res = DB.select("sessions", "*", f"session_id = '{session_id}'")
-    if not res:
-        return False
-    return res[0]
-
-def set_auth(DB, session_id, token):
-    DB.update("sessions", f"token = '{token}'", f"session_id = '{session_id}'", "login_status = 1")
-    return token
-
-def check_auth(DB, session_id, token):#TODO
-    res = DB.select("sessions", "token", f"session_id = '{session_id}'")
-    if not res or res[0][0] != token:
+    if not res or res[0][7] != ip_address or res[0][8] != user_agent:
+        clear(DB, session_id)
         return False
     return True
 
-def delete(DB, session_id):
-    DB.delete("sessions", f"session_id = '{session_id}'")
+def get(DB, session_id):
+    res = DB.select("sessions", "*", f"session_id = '{session_id}'")
+    if not res:
+        return None
+    return res[0]
+
+def set_auth(DB, session_id, token, user_id):
+    DB.update("sessions", f"token = '{token}', login_status = '1', user_id = '{user_id}'", f"session_id = '{session_id}'", "login_status = 1")
+    return True
+
+def check_auth(DB, session_id, token):#TODO
+    res = DB.select("sessions", "token, login_status, user_id", f"session_id = '{session_id}'")
+    if not res or res[0][0] != token  or not res[0][1]:
+        return False
+    res1 = DB.select("users", "token_secret", f"id = '{res[0][2]}'")
+    if not res1:
+        return False
+    return True,res[1][0]
+
+
+
+
+# def delete(DB, session_id):
+#     DB.delete("sessions", f"session_id = '{session_id}'")
